44d84588552d4f76b1bea634d310799547321d2c
Kostyukhin Alexey <121952648+poritsal@users.noreply.github.com>
2023-12-15 21:25:56 +0300

    Add files via upload

---
+++ firebase-credentials.js
@@ -0,0 +1,10 @@
+export
+const firebaseConfig = {
+    apiKey: "AIzaSyDm3BUmJZR8oU4J0XtsH6FF_yMV9asyVUo",
+    authDomain: "minilab4-53c03.firebaseapp.com",
+    projectId: "minilab4-53c03",
+    storageBucket: "minilab4-53c03.appspot.com",
+    messagingSenderId: "91582819625",
+    appId: "1:91582819625:web:deadadcf209acdd2241b38",
+    measurementId: "G-7XQT2GN03M"
+};
--- firebase.js
+++ firebase.js
@@ -1,15 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebase-credentials";
+//import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebase-credentials";
+import {initializeFirestore} from 'firebase/firestore';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
 
+// Initialize Cloud Firestore and get a reference to the service
+// Стандартный способ у меня не работал
+//const db = getFirestore(app);
+
+const db = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+});
 export { app, db, auth };
